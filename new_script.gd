57a154ddf2ac19be3facfb6168e32c05b0d3b88c
CalAmityWitchling <calamitywitch@seznam.cz>
2025-05-21 19:58:29 +0200

    conflict part 1

--- new_script.gd
+++ new_script.gd
@@ -9,6 +9,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print("Hi, Im a game!")
+	print("Hi, Im the game!")
 	
 	pass
